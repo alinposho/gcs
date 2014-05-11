e65c41f4351bf8ceaa57c8e73d283d532d76eaae
Alin Posorovaschi <alinposho@yahoo.com>
2014-05-11 13:06:55 +0100

    Possible use of Protocols to read configurations from file
     - Appears to make the code more complex

--- src/gcs/config.clj
+++ src/gcs/config.clj
@@ -3,26 +3,43 @@
   (:require [clojure.string :as str])
   (:import (java.util Properties)))
 
-(defn- load-properties [src]
+(defn load-properties [src]
   (with-open [rdr (reader src)]
     (doto (Properties.)
       (.load rdr))))
 
-(defn config
+(defn read-config
   []
   (load-properties "resources/gcs.properties"))
 
-(defn client-email [props]
-  (.get props "gcs.client_email"))
+; (defn client-email [props]
+;   (.get props "gcs.client_email"))
 
-(defn gcs-permission-scope [props]
-  (.get props "gcs.permissions.scope"))
+; (defn gcs-permission-scope [props]
+;   (.get props "gcs.permissions.scope"))
+
+(defprotocol GcsConfig
+  "A protocol for accessing application configurations"
+  (client-email [this] "Get the client email property")
+  (permission-scope [this] "Get the Google Cloud Storage permissions property"))
+
+(defrecord GcsConfig [props])
+(extend-type GcsConfig 
+  Config
+  (client-email [this]
+    (.get (:props this) "gcs.client_email"))
+  (permission-scope [this]
+    (.get (:props this) "gcs.permissions.scope")))
 
 (comment
 
 (load-file "src/gcs/config.clj")
-(refer 'gcs.config)
-(client-email (config))
-(gcs-permission-scope (config))
+(refer 'gcs.read-config)
+; (client-email (config))
+; (gcs-permission-scope (config))
+
+(def gcs-config (->GcsConfig (read-config)))
+(client-email gcs-config)
+(permission-scope gcs-config)
 
 )
